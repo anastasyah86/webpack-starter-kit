0f5ba01f8083677c4bc14c980f3433a6dba595ff
anastasyah86 <nastasyah86@gmail.com>
2021-01-14 18:43:47 +0200

    lightbox doesn't work :D

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './styles.scss';
-
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
-// console.log(basicLightbox);
+
 const openModal = document.querySelector('button[data-open-modal]');
+openModal.addEventListener('click', instance.show());
+
 const modalTemplate = document.querySelector('#modal');
 const instance = basicLightbox.create(modalTemplate, {
-  onShow: instance =>
-    (instance.element().querySelector('button[data-close-modal]').onclick =
-      instance.close),
+  onShow: instance => {
+    const close = instance.element().querySelector('button[data-close-modal]');
+    close.onclick = instance.close;
+  },
 });
-instance.show();
-
-openModal.addEventListener('click', instance.show());
+// instance.show();
